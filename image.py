5fc6c17a8572e803cadd23aacebf841c8913b2f4
blitzher <40241197+blitzher@users.noreply.github.com>
2019-10-05 11:38:30 +0200

    bug fix and update naming convention

--- image.py
+++ image.py
@@ -3,8 +3,7 @@ usage
 
 save an image to <sour.png>
 script will convert this image into a byteformat
-and save it to a python file
-name <img.png>
+and save it to a python file named <img.py>
 
 move this file into your directory
 and to write this back into an image, use
@@ -13,6 +12,7 @@ from img import data
 puzzle_tools.write_png(data, (size_x, size_y))
 
 after compiling to a .exe, the bytefile will be unnecessary
+run this file after making the 
 """
 
 
@@ -28,15 +28,15 @@ t_quote = r'"""'
 py_info = "%s %s %s %s" % ('data = ', t_quote, str(byte_pack), t_quote)
 
 try:
-	os.remove('conv.py')
+	os.remove('img.py')
 except: pass;
 
-with open("conv.py", 'w') as f:
+with open("img.py", 'w') as f:
 	f.write(py_info)
 
 try:
-	from conv import data
+	from img import data
 	im = Image.frombytes('RGB', (50,50), byte_pack)
 	im.save('img.png')
 except Exception as e:
-	print(e)
+	pass
